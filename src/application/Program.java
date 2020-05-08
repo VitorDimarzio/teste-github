0f8b54103407a155cb1044136a133a1a7fe3e8d7
Vitor Dimarzio <vitor.adimarzio@gmail.com>
2020-05-08 14:44:29 -0300

    Acrescentado o Scanner sc

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
 
 	}
 
